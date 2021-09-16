f941bc1bff75c63786868c3a4ae9fde2b8c5981b
azharjk <azhr.project@gmail.com>
2021-09-16 15:55:49 +0700

    lms_backend: added user entity

---
+++ lms_backend/src/main/java/com/azharjk/lms_backend/user/User.java
@@ -0,0 +1,56 @@
+package com.azharjk.lms_backend.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false)
+  private String fullname;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFullname() {
+    return fullname;
+  }
+
+  public void setFullname(String fullname) {
+    this.fullname = fullname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
