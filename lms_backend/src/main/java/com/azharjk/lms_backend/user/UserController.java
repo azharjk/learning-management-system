23162960dc7610deb411cdb8d04ff42ac74443b1
azharjk <azhr.project@gmail.com>
2021-09-19 14:34:24 +0700

    lms_backend(not tested): created user rest basic crud
    added to UserController class basic crud functionality

--- lms_backend/src/main/java/com/azharjk/lms_backend/user/UserController.java
+++ lms_backend/src/main/java/com/azharjk/lms_backend/user/UserController.java
@@ -2,8 +2,16 @@ package com.azharjk.lms_backend.user;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +26,26 @@ public class UserController {
   @GetMapping
   public List<User> getAllUsers() {
     return userService.getAllUsers();
+  }
+
+  @GetMapping("/{userId}")
+  public User getUserById(@PathVariable Long userId) {
+    return userService.getUserById(userId);
+  }
+
+  @PostMapping
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{userId}")
+  public User editUser(@RequestBody User user, @PathVariable Long userId) {
+    return userService.editUser(user, userId);
+  }
+
+  @DeleteMapping("/{userId}")
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public void deleteUser(@PathVariable Long userId) {
+    userService.deleteUser(userId);
   }
 }
