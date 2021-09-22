1a7e35b4f04215216feb94d450ebd23c905d4331
azharjk <azhr.project@gmail.com>
2021-09-22 08:27:08 +0700

    lms_backend: refactor to lombok

--- lms_backend/src/main/java/com/azharjk/lms_backend/user/User.java
+++ lms_backend/src/main/java/com/azharjk/lms_backend/user/User.java
@@ -6,8 +6,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   @Id
   @GeneratedValue
@@ -22,52 +29,9 @@ public class User {
   @Column(nullable = false)
   private String password;
 
-  public User() {
-
-  }
-
   public User(String fullname, String email, String password) {
     this.fullname = fullname;
     this.email = email;
-    this.password = password;
-  }
-
-  public User(Long id, String fullname, String email, String password) {
-    this.id = id;
-    this.fullname = fullname;
-    this.email = email;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getFullname() {
-    return fullname;
-  }
-
-  public void setFullname(String fullname) {
-    this.fullname = fullname;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
     this.password = password;
   }
 }
