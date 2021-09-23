6cd7e4f29032f3d7a50cd1d85de63fc5b02ccfbc
azharjk <azhr.project@gmail.com>
2021-09-23 17:43:48 +0700

    lms_backend: enable cors origin

--- lms_backend/src/main/java/com/azharjk/lms_backend/LmsBackendApplication.java
+++ lms_backend/src/main/java/com/azharjk/lms_backend/LmsBackendApplication.java
@@ -2,12 +2,23 @@ package com.azharjk.lms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LmsBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LmsBackendApplication.class, args);
 	}
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer(){
+      @Override
+      public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**").allowedOrigins("http://localhost:3000");
+      }
+    };
+  }
 }
