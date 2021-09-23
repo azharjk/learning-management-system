8afc0e04ff7c5a0df2c8ac9ae23cc862edcbfbed
azharjk <azhr.project@gmail.com>
2021-09-23 09:42:40 +0700

    lms_backend(tested): added dirties context for reload

--- lms_backend/src/test/java/com/azharjk/lms_backend/user/UserControllerTest.java
+++ lms_backend/src/test/java/com/azharjk/lms_backend/user/UserControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestPropertySource(locations = "classpath:application-test.properties")
+@DirtiesContext
 public class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
@@ -38,7 +40,7 @@ public class UserControllerTest {
     userSeeder.seed(2);
   }
 
-  public String objectToJson(User user) throws Exception {
+  private String objectToJson(User user) throws Exception {
     ObjectMapper objectMapper = new ObjectMapper();
     String json = objectMapper.writeValueAsString(user);
 
